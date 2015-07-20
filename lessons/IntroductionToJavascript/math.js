41df6188f09bf03a3035b0f72a38a96d58a5d21f
subhasisghosal <subhasis@fusioncharts.com>
2015-07-20 16:55:50 +0530

    Javascript problem day 1 uploaded

---
+++ lessons/IntroductionToJavascript/math.js
@@ -0,0 +1,60 @@
+function getRandNum() {
+	var numbers = [];
+	for (var i = 0; i < 80; i++) {
+		var n = Math.floor(Math.random()*100);
+		if(n>9)
+			numbers.push(n);	
+	};
+	return numbers;
+}
+
+function sort(arr) {
+	for (var i = 0; i < arr.length; i+=2) {
+		if(arr[i+1]>arr[i]){
+			arr[i+1] = arr[i+1] + arr[i];
+			arr[i] = arr[i+1] - arr[i];
+			arr[i+1] = arr[i+1] - arr[i];
+		}
+	};
+	return arr;
+}
+
+var ans = [];
+
+function subtractions(){
+	var sum = "";
+	var sortedList = sort(getRandNum());
+	ans.length = 0;
+	for (var i = 0, j = 0; i < 30; i++, j+=2) {
+		sum+="<div class='sum' id='d"+i+"'>";
+		sum+="<div>"+sortedList[j]+"</div>";
+		sum+="<div>-"+sortedList[j+1]+"</div>";
+		sum+="<div class='answer'><input id='"+i+"' type='text' onkeyup='validate(this.id)'/></div></div>"
+		ans.push(parseInt(sortedList[j])-parseInt(sortedList[j+1]));
+	};
+	document.getElementById("a").innerHTML = sum;
+	//document.getElementById("b").innerHTML = ans;
+}
+
+function additions(){
+	var sum = "";
+	var list = getRandNum();
+	ans.length = 0;
+	for (var i = 0, j = 0; i < 30; i++, j+=2) {
+		sum+="<div class='sum' id='d"+i+"'>";
+		sum+="<div>"+list[j]+"</div>";
+		sum+="<div>+"+list[j+1]+"</div>";
+		sum+="<div class='answer'><input id='"+i+"' type='text' onkeyup='validate(this.id)'/></div></div>"
+		ans.push(parseInt(list[j])+parseInt(list[j+1]));
+	};
+	document.getElementById("a").innerHTML = sum;
+	//document.getElementById("b").innerHTML = ans;
+}
+
+function validate(id){
+	var box = document.getElementById(id);
+	if(parseInt(box.value) === ans[id])
+		box.style.background = "#7BFF6F";
+	else
+		box.style.background = "#FF5050";
+}
