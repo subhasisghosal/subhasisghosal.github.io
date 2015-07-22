87fa9f9d20cf4f11d884ad71ebdacc45e1ca4bee
subhasisghosal <subhasis@fusioncharts.com>
2015-07-22 08:01:15 +0530

    Memory feature added & % is partly working

--- lessons/IntroductionToJavascript/calculator.js
+++ lessons/IntroductionToJavascript/calculator.js
@@ -1,6 +1,6 @@
 var op = document.getElementById("screen");
 
-var var1, var2, result;
+var var1, var2, result, memory=0;
 
 function putValue(val){
 	op.selectionStart = op.selectionEnd = 0;
@@ -8,9 +8,46 @@ function putValue(val){
 }
 
 function allClear(v){
-	op.value = (v==0) ? "" : op.value.slice(0,-1);
+	op.value = (v==0) ? "" : op.value.slice(0,-v);
 }
 
 function evalue(){
 	op.value = eval(op.value);
+}
+
+function mem(v){
+	switch(v){
+		case '+': memory += op.value;
+				op.value = "";
+		break;
+		case '-': memory -= op.value;
+				op.value = "";
+		break;
+		case 'r': op.value += memory;
+		break;
+		case 'c': memory = 0;
+		break;
+	}
+}
+
+function percent(){
+	var a,p='';
+	if(op.value.length===1){
+		p = op.value;
+	}
+	else{
+		for (var i = 0; p.length < 2; i++) {
+				a = op.value.substring(op.value.length-i-1,op.value.length-i);
+				if (a>=0 && a<=9)
+					p = a + p;
+				else
+					break;
+				console.log(a);
+		}
+	}
+	alert(p+"%");
+	p = parseInt(p)/100;
+	console.log(p);
+	allClear(2);
+	op.value += p;
 }
