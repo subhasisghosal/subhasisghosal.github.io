9156cf20fbf98cef321ebf5ef6fd6000fc592ca0
subhasisghosal <subhasis@fusioncharts.com>
2015-07-29 17:13:09 +0530

    Validation working

--- lessons/IntroductionToPHP/submit.php
+++ lessons/IntroductionToPHP/submit.php
@@ -4,12 +4,12 @@
 		$interest = array();
 		$flag = false;
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			$flag = true;
 		  if (empty($_POST["name"])) {
 		    $nameErr = "Missing";
 		    $flag = false;
 		  } else {
 		    $name = $_POST["name"];
-		    $flag = true;
 		  }
 		  if (empty($_POST["email"])) {
 		    $emailErr = "Missing";
@@ -20,9 +20,6 @@
 		    	$emailErr = "Wrong";
 		    	$flag = false;
 		  	}
-		    else{
-		    	$flag = true;
-		    }
 		  }
 		  if (empty($_POST["phone"])) {
 		    $phoneErr = "Missing";
@@ -32,50 +29,39 @@
 		  	if((preg_match("/[^0-9]/", $phone, $match)) || strlen($phone) != 10){
 		  		$phoneErr = "Invalid";
 		  		$flag = false;
-		  	}
-		  	else{	
-		    	$flag = true;
-		  	}    
+		  	}   
 		  }
 		  if (empty($_POST["sex"])) {
 		    $sexErr = "Missing";
 		    $flag = false;
 		  } else {
 		    $sex = $_POST["sex"];
-		    $flag = true;
 		  }
 		  if (empty($_POST["interest"])) {
 		    $interestErr = "Missing";
 		    $flag = false;
 		  } else {
 		    $interest = $_POST["interest"];
-		    $flag = true;
 		  }
 		  if (empty($_POST["country"])) {
 		    $countryErr = "Missing";
 		    $flag = false;
 		  } else {
 		    $country = $_POST["country"];
-		    $flag = true;
 		  }
 		  if (empty($_POST["state"])) {
 		    $stateErr = "Missing";
 		    $flag = false;
 		  } else {
 		    $state = $_POST["state"];
-		    $flag = true;
 		  }
 		  if (empty($_POST["address"])) {
 		    $addressErr = "Missing";
 		    $flag = false;
 		  } else {
 		    $address = $_POST["address"];
-		    $flag = true;
 		  }
 		}
-		print_r($_POST);
-		#echo $name, $email, $phone, $sex, $country, $state, $address;
-		
 		if($flag==true){
 			foreach ($interest as $value) {
 				$int .= $value.", ";
